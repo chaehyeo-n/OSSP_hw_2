27f51b855861741ae076954f5c19d3aa976f322f
chaehyeo-n <139209815+chaehyeo-n@users.noreply.github.com>
2024-03-27 00:40:23 +0900

    Add files via upload
    2024-03-20 OSSP 개인과제#2

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name, number;
+	cout << "이름을 입력하세요: ";
+	cin >> name;
+	cout << "학번을 입력하세요: ";
+	cin >> number;
+	
+	cout << endl << "<출력>" << endl;
+	cout << "이름: " << name << endl;
+	cout << "학번: " << number << endl;
+}
